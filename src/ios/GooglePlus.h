bfa10bee3841c2b91e744e0d2f6cb18adb5c6151
jvictorm13 <jvictorgmodesto@gmail.com>
2025-07-12 16:03:00 -0300

    update versions and fix

--- src/ios/GooglePlus.h
+++ src/ios/GooglePlus.h
@@ -6,10 +6,10 @@
 @property(nonatomic, copy) NSString *callbackId;
 
 // Métodos expostos ao JS
-- (void)isAvailable:(CDVInvokedUrlCommand *)command;
-- (void)login:(CDVInvokedUrlCommand *)command;
-- (void)trySilentLogin:(CDVInvokedUrlCommand *)command;
-- (void)logout:(CDVInvokedUrlCommand *)command;
-- (void)disconnect:(CDVInvokedUrlCommand *)command;
+- (void)isAvailable:(CDVInvokedUrlCommand*)command;
+- (void)login:(CDVInvokedUrlCommand*)command;
+- (void)trySilentLogin:(CDVInvokedUrlCommand*)command;
+- (void)logout:(CDVInvokedUrlCommand*)command;
+- (void)disconnect:(CDVInvokedUrlCommand*)command;
 
 @end
--- src/ios/GooglePlus.m
+++ src/ios/GooglePlus.m
@@ -1,12 +1,9 @@
-#import "AppDelegate.h"
-#import "objc/runtime.h"
 #import "GooglePlus.h"
 @import GoogleSignIn;
 
 @implementation GooglePlus
 
-- (void)pluginInitialize
-{
+- (void)pluginInitialize {
     NSLog(@"GooglePlus pluginInitialize");
 }
 
@@ -19,7 +16,7 @@
     self.callbackId = command.callbackId;
     NSDictionary* options = command.arguments[0];
 
-    NSString* clientId = [self getClientId];
+    NSString* clientId       = [self getClientId];
     NSString* serverClientId = options[@"webClientId"];
 
     GIDConfiguration* config = serverClientId.length > 0
@@ -27,8 +24,8 @@
       : [[GIDConfiguration alloc] initWithClientID:clientId];
 
     [GIDSignIn.sharedInstance signInWithConfiguration:config
-                            presentingViewController:self.viewController
-                                            completion:^(GIDGoogleUser * _Nullable user, NSError * _Nullable error) {
+                              presentingViewController:self.viewController
+                                              completion:^(GIDGoogleUser * _Nullable user, NSError * _Nullable error) {
         [self handleSignIn:user error:error];
     }];
 }
@@ -46,42 +43,43 @@
 
 - (void)logout:(CDVInvokedUrlCommand*)command {
     [GIDSignIn.sharedInstance signOut];
-    CDVPluginResult* res = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:@"logged out"];
+    CDVPluginResult* res = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK
+                                             messageAsString:@"logged out"];
     [self.commandDelegate sendPluginResult:res callbackId:command.callbackId];
 }
 
 - (void)disconnect:(CDVInvokedUrlCommand*)command {
     [GIDSignIn.sharedInstance disconnectWithCompletion:^(NSError * _Nullable error) {
         CDVPluginResult* res = error
-            ? [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString:error.localizedDescription]
-            : [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:@"disconnected"];
+          ? [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString:error.localizedDescription]
+          : [CDVPluginResult resultWithStatus:CDVCommandStatus_OK   messageAsString:@"disconnected"];
         [self.commandDelegate sendPluginResult:res callbackId:command.callbackId];
     }];
 }
 
 - (void)handleSignIn:(GIDGoogleUser*)user error:(NSError*)error {
     if (error || !user) {
-        CDVPluginResult* res = [CDVPluginResult
-            resultWithStatus:CDVCommandStatus_ERROR
-              messageAsString:(error.localizedDescription ?: @"Sign-in failed")];
+        CDVPluginResult* res = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR
+                                                  messageAsString:(error.localizedDescription ?: @"Sign-in failed")];
         [self.commandDelegate sendPluginResult:res callbackId:self.callbackId];
         return;
     }
 
     NSDictionary* result = @{
-        @"email":           user.profile.email ?: [NSNull null],
-        @"idToken":         user.authentication.idToken ?: [NSNull null],
-        @"accessToken":     user.authentication.accessToken ?: [NSNull null],
-        @"refreshToken":    user.authentication.refreshToken ?: [NSNull null],
-        @"serverAuthCode":  user.serverAuthCode ?: [NSNull null],
-        @"userId":          user.userID ?: [NSNull null],
-        @"displayName":     user.profile.name ?: [NSNull null],
-        @"givenName":       user.profile.givenName ?: [NSNull null],
-        @"familyName":      user.profile.familyName ?: [NSNull null],
-        @"imageUrl":        ([user.profile imageURLWithDimension:120].absoluteString ?: [NSNull null])
+        @"email"          : user.profile.email           ?: [NSNull null],
+        @"idToken"        : user.authentication.idToken   ?: [NSNull null],
+        @"accessToken"    : user.authentication.accessToken ?: [NSNull null],
+        @"refreshToken"   : user.authentication.refreshToken?: [NSNull null],
+        @"serverAuthCode": user.serverAuthCode           ?: [NSNull null],
+        @"userId"         : user.userID                   ?: [NSNull null],
+        @"displayName"    : user.profile.name             ?: [NSNull null],
+        @"givenName"      : user.profile.givenName        ?: [NSNull null],
+        @"familyName"     : user.profile.familyName       ?: [NSNull null],
+        @"imageUrl"       : ([user.profile imageURLWithDimension:120].absoluteString ?: [NSNull null])
     };
 
-    CDVPluginResult* res = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsDictionary:result];
+    CDVPluginResult* res = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK
+                                           messageAsDictionary:result];
     [self.commandDelegate sendPluginResult:res callbackId:self.callbackId];
 }
 
